48b50abd6a8b3021d3d04ad3613cef7849647301
Lucas Eduardo Pereira <lucaseduardodev@outlook.com>
2023-09-06 13:27:32 -0300

    Style added to the nav and the side bar

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent {
+  checked: boolean = true;
 
+  handleClick(): void {
+    console.log('oi;');
+  }
 }
